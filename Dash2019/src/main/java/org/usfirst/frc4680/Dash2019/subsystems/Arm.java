8bd1077f9cb2dc3463e67efa512bea84e71c7ba4
unknown <antonio.pollard@chavezwaterman.com>
2019-02-14 19:59:14 -0500

    getting arm angle to work -- MUST handle initial arm placement correctly the arm has about 9 degrees of give either way

--- Dash2019/src/main/java/org/usfirst/frc4680/Dash2019/subsystems/Arm.java
+++ Dash2019/src/main/java/org/usfirst/frc4680/Dash2019/subsystems/Arm.java
@@ -43,7 +43,8 @@ public class Arm extends Subsystem {
     public static final double MINIMUM_ANGLE = -30.0;
     public static final double MAXIMUM_ANGLE = 100.0;
     public static final double ANGLE_TOLERANCE = 3.0;
-    private static final double degreesPerEncoderCount = (360.0 / 4096) * (12/60); 
+    public static final double STARTINGANGLE = 90.0;
+    private static final double degreesPerEncoderCount = (360.0 / 4096) * (12.0/60); 
 
     private PIDSourceTalon pivotMotorA;
     private WPI_TalonSRX pivotMotorB;
@@ -130,7 +131,7 @@ public class Arm extends Subsystem {
 
         public double getAngle() {
             int quadraturePosition = getSensorCollection().getQuadraturePosition();
-            return (quadraturePosition * degreesPerEncoderCount) + MINIMUM_ANGLE;
+            return STARTINGANGLE - (quadraturePosition * degreesPerEncoderCount);
         }   
     }
 
