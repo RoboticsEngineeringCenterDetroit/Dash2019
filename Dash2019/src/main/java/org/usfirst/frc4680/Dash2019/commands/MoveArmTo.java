604108417debbf4ee1b67e5204c33410175eb6e4
unknown <antonio.pollard@chavezwaterman.com>
2019-02-17 17:10:47 -0500

    Limit speeds of PID arm commands. Make a basic auton mode

--- Dash2019/src/main/java/org/usfirst/frc4680/Dash2019/commands/MoveArmTo.java
+++ Dash2019/src/main/java/org/usfirst/frc4680/Dash2019/commands/MoveArmTo.java
@@ -8,12 +8,26 @@
 package org.usfirst.frc4680.Dash2019.commands;
 
 import org.usfirst.frc4680.Dash2019.Robot;
+import org.usfirst.frc4680.Dash2019.subsystems.Arm;
+import org.usfirst.frc4680.Dash2019.subsystems.ArmExtender;
+
+import edu.wpi.first.wpilibj.PIDBase;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class MoveArmTo extends Command   {
   double m_targetAngle;
+  double m_targetLength;
+
+  private static final double DELTA_ANGLE_MAX = (Arm.MAXIMUM_ANGLE - Arm.MINIMUM_ANGLE) / 4.0;
+  private static final double DELTA_LENGTH_MAX = (ArmExtender.MAX_LENGTH - ArmExtender.MIN_LENGTH) / 4.0;
+
   public MoveArmTo(double targetAngle) {
+    this(targetAngle, Robot.armExtender.getLength());
+  }
+
+  public MoveArmTo(double targetAngle, double targetLength) {
     m_targetAngle = targetAngle;
+    m_targetLength = targetLength;
     // Use requires() here to declare subsystem dependencies
     requires(Robot.arm);
   }
@@ -21,18 +35,25 @@ public class MoveArmTo extends Command   {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    Robot.arm.setPosition(m_targetAngle);
+    m_targetAngle = clamp(m_targetAngle, Arm.MINIMUM_ANGLE, Arm.MAXIMUM_ANGLE);
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double angleDelta = m_targetAngle - Robot.arm.getAngle();
+    angleDelta = clamp(angleDelta, -DELTA_ANGLE_MAX, DELTA_ANGLE_MAX);
+    Robot.arm.setPosition(Robot.arm.getAngle() + angleDelta);
+
+    double lenDelta = m_targetLength - Robot.armExtender.getLength();
+    lenDelta = clamp(lenDelta, -DELTA_LENGTH_MAX, DELTA_LENGTH_MAX);
+    Robot.armExtender.setLength(m_targetLength);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return Robot.arm.isAtSetpoint();
+    return Robot.arm.isAtSetpoint() && Robot.armExtender.isAtSetpoint();
   }
 
   // Called once after isFinished returns true
@@ -45,5 +66,11 @@ public class MoveArmTo extends Command   {
   @Override
   protected void interrupted() {
     Robot.arm.stop();
+    Robot.armExtender.stop();
   }
+
+  private static double clamp(double value, double low, double high) {
+    return Math.max(low, Math.min(value, high));
+  }
+
 }
